27bb3a5d76ab351fb232a9c399fa3b4a92d40366
pnowicki235078 <49626497+pnowicki235078@users.noreply.github.com>
2019-04-24 04:47:41 +0200

    Add files via upload
    Dodanie walidacji przy tworzeniu.
    Sprawdzane jest czy wydarzenie obejmuje tylko jeden dzień, czy nie trwa krócej niż 15 minut, oraz czy nie godzina rozpoczęcia nie jest wcześniejsza niż godzina zakończenia.

--- app/src/main/java/com/example/cezary/przykladowewidoki/CreateEventActivity.java
+++ app/src/main/java/com/example/cezary/przykladowewidoki/CreateEventActivity.java
@@ -5,10 +5,16 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
@@ -23,6 +29,14 @@ public class CreateEventActivity extends AppCompatActivity {
     TimePickerDialog timePickerDialog;
     int sYear, sMonth, sDay, sHour, sMinute,
         eYear, eMonth, eDay, eHour, eMinute;
+
+    int rnd(int minute){
+        float min = minute/5.0f;
+        Math.round(min);
+        minute = (int) min * 5;
+        return minute;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +63,7 @@ public class CreateEventActivity extends AppCompatActivity {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         sHour = hourOfDay;
-                        sMinute = minute;
+                        sMinute = rnd(minute);
                         mStartTimeTXT.setText(String.format("%02d:%02d", hourOfDay, minute));
                     }
                 }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
@@ -63,13 +77,16 @@ public class CreateEventActivity extends AppCompatActivity {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         eHour = hourOfDay;
-                        eMinute = minute;
+
+                        eMinute = rnd(minute);
                         mEndTimeTXT.setText(String.format("%02d:%02d", hourOfDay, minute));
                     }
                 }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
                 timePickerDialog.show();
+
             }
         });
+
 
         mStartDateTXT.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,6 +122,33 @@ public class CreateEventActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (eHour < sHour || (eHour == sHour && eMinute < sMinute + 15)) {
+                    timePickerDialog = new TimePickerDialog(CreateEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                            eHour = hourOfDay;
+                            eMinute = rnd(minute);
+                            mEndTimeTXT.setText(String.format("%02d:%02d", hourOfDay, minute));
+                        }
+                    }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+                    timePickerDialog.show();
+                    return;
+                }
+
+                if (sDay != eDay || sMonth != eMonth || eYear != sYear){
+                    datePickerDialog = new DatePickerDialog(CreateEventActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            eYear = year;
+                            eMonth = month;
+                            eDay = dayOfMonth;
+                            mEndDateTXT.setText(dayOfMonth + "/" + month + "/" + year);
+                        }
+                    }, calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+                    datePickerDialog.show();
+                    return;
+                }
+
                 Intent data = getIntent();
                 data.putExtra("name", mNameTXT.getText().toString());
                 data.putExtra("place", mPlaceTXT.getText().toString());
